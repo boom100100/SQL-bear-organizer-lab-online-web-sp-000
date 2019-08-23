878f4a77b1d8c12c7e44d83fe539cdd32ff362cb
Bernadette Davis <bsd231@nyu.edu>
2019-08-23 19:04:03 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   #"SELECT MAX(bears.color) FROM (SELECT COUNT(*) AS num FROM bears) bears;"
-  "SELECT MAX(color), COUNT((SELECT * FROM bears WHERE color = \"dark brown\")) FROM bears;"
+  "SELECT MAX(color), COUNT(*) FROM bears WHERE color = \"dark brown\";"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
